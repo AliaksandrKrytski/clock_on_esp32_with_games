4b4f74c637b29ad8a36bb6c12ad4fa8536d112ed
AliaksandrKrytski <forgithub159@gmail.com>
2026-09-10 20:44:51 +0300

    First experience with interrupts(bad experience)

--- project/project.ino
+++ project/project.ino
@@ -1,4 +1,4 @@
-#include "Wire.h"
+/*#include "Wire.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
@@ -23,4 +23,44 @@ void loop() {
   display.print("Screen      test"); 
   display.display();          
   delay(1000);   
+}
+*/
+
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+#define OLED_SDA 5
+#define OLED_SCL 4
+#define OLED_RST 16
+#define Button_Pin 13
+
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST);
+
+void setup() {
+  pinMode(Button_Pin, OUTPUT);
+  attachInterrupt(0, printButtonInterrupt, RISING);
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for(;;); 
+  }
+}
+
+void loop() {
+  digitalWrite(Button_Pin, HIGH);
+  display.clearDisplay();   
+  display.setTextSize(2);      
+  display.setTextColor(WHITE);
+  display.setCursor(20, 20);
+  display.print("sreen test?"); 
+  display.display();
+}
+
+void printButtonInterrupt(){
+  display.clearDisplay();   
+  display.setTextSize(2);      
+  display.setTextColor(WHITE);
+  display.setCursor(20, 20);
+  display.print("Interrupt!"); 
+  display.display();          
 }
